da76d4560309503d65e3ebde1b6d67a2c99c1a76
Satria Amru Dito <developerdito@email.com>
2022-03-30 08:52:03 +0700

    aaaaa

--- profile.js
+++
@@ -1,8 +0,0 @@
-var socket = new io();
-
-function profile() {
-    var username = document.getElementById("username");
-    var email = document.getElementById("email");
-    var phone = document.getElementById("phone");
-    socket.emit("profile", username.value, email.value, phone.value);
-}
