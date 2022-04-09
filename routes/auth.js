1fbd38c1f11d1387f2e16174139535b7c1d2b92b
rizkykamil <rabbaniikamil@gmail.com>
2022-04-09 09:28:34 +0700

    tes dulu

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/AuthController');
-
-router.post('/register', authController.register)
-router.post('/login', authController.login)
-
-model.export = router
